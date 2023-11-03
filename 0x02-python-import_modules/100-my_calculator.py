43bc32fcbbd2f772a2c7c03a5428373e7a00d102
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-03 14:22:15 +0200

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(av) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = int(av[1))
+    a = int(av[1])
     b = int(av[3])
     op = av[2]
     if op == "+":
