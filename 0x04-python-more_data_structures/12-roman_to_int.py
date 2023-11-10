9d400c36cc5e2aa267deb4e1e09c1b7929b1e600
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-10 22:42:22 +0200

    twelve

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,9 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    i = 0
+    sums = 0
     num = 0
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman:
-            num += roman[s[i:i+2]]
-            i += 2
-        else:
-            num + = roman[s[i]]
-            i += 1
-    return num
+    for x in reversed(roman_string):
+        num = roman[x]
+        total += num if total < num * 5 else -num
+    return total
