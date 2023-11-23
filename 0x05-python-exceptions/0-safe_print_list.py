7e798c51b36831394fd7976576a0e77a9192c6dc
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-23 02:11:34 +0200

    zero

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    adds = 0
+    try:
+        while adds < x:
+            print(my_list[adds], end='')
+            adds += 1
+        except:
+            None
+        print()
+        return adds
