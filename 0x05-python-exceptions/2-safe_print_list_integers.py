e7502543ce8ad07c56b56ccc26c58ddef8224264
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-23 13:00:47 +0200

    two

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
+    i = 0
     try:
-        while num < x:
+        while i < x:
             if type(my_list[num]) is int:
                 print("{:d}".format(my_list[num]), end='')
-            num += 1
+                num += 1
+            i += 1
     except IndexError:
         None
     print()
