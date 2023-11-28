1b87405996e61ba73c44365f6695a0ddfa829a0f
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-28 04:31:04 +0200

    seven

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
             return out
         for i in range(self.__height):
             for j in range(self.__width):
-                out += self.print_symbol
+                out += str(self.print_symbol)
             if i < self.__height - 1:
                 out += "\n"
         return out
