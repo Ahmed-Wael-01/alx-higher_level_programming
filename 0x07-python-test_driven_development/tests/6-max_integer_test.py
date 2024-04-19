7daa62f0be2970d07edb72f5953d1095fd1a650b
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-19 21:02:35 +0200

    finish tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -18,6 +18,25 @@ class TestMaxInteger(unittest.TestCase):
         """trying normal case"""
         self.assertEqual(max_integer([]), None)
 
+    def test_first(self):
+        """trying normal case"""
+        self.assertEqual(max_integer([8, 2, 5]), 8)
+
+    def test_mid(self):
+        """trying normal case"""
+        self.assertEqual(max_integer([1, 9, 5]), 9)
+
+    def test_niga(self):
+        """trying normal case"""
+        self.assertEqual(max_integer([-1, 2, 5]), 5)
+
+    def test_all_nega(self):
+        """trying normal case"""
+        self.assertEqual(max_integer([-1, -2, -5]), -1)
+
+    def test_one(self):
+        """trying normal case"""
+        self.assertEqual(max_integer([1]), 1)
 
 if __name__ == '__main__':
     unittest.main()
