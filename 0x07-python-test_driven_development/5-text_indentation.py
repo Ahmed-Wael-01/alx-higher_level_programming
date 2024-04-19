4abe12aeb337d8b1fbd7bf21cbb6fb7d8e58cd96
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-19 20:15:08 +0200

    text test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""indent a paragraph"""
+
+def text_indentation(text):
+    """prints a paragraph after putting 2 new lines after each . ? :"""
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
+    for delim in ".?:":
+        text = (delim + "\n\n").join(
+                [line.strip(" ") for line in text.split(delim)])
+    print(text, end="")
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
