13243e7d313112df1deec0475eda96bcbc3582bc
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-19 20:40:11 +0200

    unit testing

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""testing MAX method"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """testing every case in max integer method"""
+    def test_normal(self):
+        """trying normal case"""
+        self.assertEqual(max_integer([1, 2, 5]), 5)
+
+    def test_no_args(self):
+        """trying normal case"""
+        self.assertEqual(max_integer(), None)
+
+    def test_empty(self):
+        """trying normal case"""
+        self.assertEqual(max_integer([]), None)
+
+
+if __name__ = '__main__':
+    unittest.main()
