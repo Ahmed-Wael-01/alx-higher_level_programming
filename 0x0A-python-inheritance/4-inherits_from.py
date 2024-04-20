be0ace4b34f5a710bbd686094776e5986f13b2d2
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-20 21:54:21 +0200

    class checkers

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """checks something i guess"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
