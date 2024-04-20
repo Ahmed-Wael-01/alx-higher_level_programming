c1829c9d8084a4b1269639980d9db9d9386935cd
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-20 21:30:25 +0200

    subclass checker

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """checks if obj is a subclass of a_class"""
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
