6be2aee1114e9c9c703299b4b02e0d19b6804400
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-21 16:29:52 +0200

    exception in area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""improved geometry class"""
+
+
+class BaseGeometry():
+    """raise exception in area"""
+    def area(self):
+        """it's still under work"""
+        raise Exception("area() is not implemented")
