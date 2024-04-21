24f23d11df2fb3618f88444c906f259f8b0d685b
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-21 18:02:43 +0200

    square improved

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""simple square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """making a cute square"""
+    def __init__(self, size):
+        """init a square"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calc area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """description"""
+        return "[Square] {0}/{0}".format(self.__size)
