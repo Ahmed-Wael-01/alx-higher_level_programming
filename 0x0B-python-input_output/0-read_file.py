38a01a90fd66c3e5de20c61f7adfb41f2dc0cfd8
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-21 22:35:25 +0200

    read only

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """reading files all day long"""
     with open(filename, encoding="utf-8") as fl:
         text = fl.read()
-        print(text)
+        print(text, end='')
