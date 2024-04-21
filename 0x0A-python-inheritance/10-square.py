6c038d1fcba04ea1db728fe8d2b5cb7614b91ad0
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-21 17:55:29 +0200

    square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""simple square class"""
+
+
+Rectangle = __import__('9-rectangle').Recrangle
+
+
+Square(Rectangle):
+    """making a cute square"""
+    def __init__(self, size):
+        """init a square"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calc area"""
+        return self.__size ** 2
