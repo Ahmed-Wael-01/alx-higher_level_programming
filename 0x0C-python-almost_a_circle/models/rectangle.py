f287d44348859d5c95178cf8d35ba4fc8472958d
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-23 01:04:51 +0200

    improved display rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,9 +91,9 @@ class Rectangle(Base):
 
     def display(self):
         """it displays the rectangle in high quality"""
-        print('\n' * self.__y)
+        print('\n' * self.__y, end='')
         for x in range(self.__height):
-            print(' ' * self.__x)
+            print(' ' * self.__x, end='')
             for j in range(self.__width):
                 print("#", end='')
             print()
