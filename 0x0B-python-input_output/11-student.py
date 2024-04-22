3adaefc9f939c5746aaa17f75481ae6b24888cd7
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-22 02:04:42 +0200

    replaces json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""student to json?!!"""
+
+
+class Student():
+    """creates students"""
+    def __init__(self, first_name, last_name, age):
+        """init a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns json"""
+        if type(attrs) is not list:
+            return self.__dict__
+        for x in attrs:
+            if type(x) is not str:
+                return self.__dict__
+        new_dict = dict()
+        for key, val in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = val
+        return new_dict
+    
+    def reload_from_json(self, json):
+        """replace attributes"""
+        json[first_name] = self.first_name
+        json[last_name] = self.last_name
+        json[age] = self.age
