e040869352a830de548f7a1b49d354add43ffe27
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-22 22:48:04 +0200

    base in model

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""base class file"""
+
+
+class Base():
+    """the base and root class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize and setting id"""
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""rectangle class"""
+
+
+class Rectangle(Base):
+    """making rectangle objects"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """makes a new rectangle"""
+        super().__init__(self, id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @proberty
+    def width(self):
+        """gets the width"""
+        return __width
+
+    @proberty
+    def height(self):
+        """gets the height"""
+        return __height
+
+    @proberty
+    def x(self):
+        """gets x"""
+        return __x
+
+    @proberty
+    def y(self):
+        """gets y"""
+        return __y
+
+    @width.setter
+    def width(self, width):
+        """sets the width"""
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        """sets the height"""
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        """sets x"""
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        """sets y"""
+        self.__y = y
