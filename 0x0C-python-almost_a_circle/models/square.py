94645187485d92d297f227064b6d0295ebbfa731
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-23 03:04:18 +0200

    rect dict

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,8 +23,8 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """updates values of the rectangle"""
