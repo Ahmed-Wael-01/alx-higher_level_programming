75e69ecc4d559791ca8fb1201a9e3779a1893452
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-23 03:22:41 +0200

    rect dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns json"""
-        if list_dictionaries is None or len(list_dictionaries) is 0:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
