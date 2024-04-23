686d4f9064d88986b9639df211bb2fbe9f50dbca
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-04-23 02:06:10 +0200

    improved r update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,17 +111,17 @@ class Rectangle(Base):
                 self.__x = kwargs["x"]
             if "y" in kwargs:
                 self.__y = kwargs["y"]
-            return
-        if len(args) >= 1:
-            self.__id = args[0]
-        if len(args) >= 2:
-            self.__width = args[1]
-        if len(args) >= 3:
-            self.__height = args[2]
-        if len(args) >= 4:
-            self.__x = args[3]
-        if len(args) >= 5:
-            self.__y = args[4]
+        elif args and len(args) > 0:
+            if len(args) >= 1:
+                self.__id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
