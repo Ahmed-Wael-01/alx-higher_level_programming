36be84a7ebd18dbf87efc6d543963538ae675c4b
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-06-11 15:11:33 +0300

    three

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- creates a table
-CREATE table IF NOT EXISTS force_name VALUES(id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name VALUES(id INT, name VARCHAR(256) NOT NULL);
