c7fa99ed67d064acd9f5970619d8b4a744174e0e
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-06-11 15:40:29 +0300

    seven

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- creates city table inside usa db
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-CREATE table IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE table IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 		state_id INT NOT NULL FOREIGN KEY REFERENCES state(id),
 		name VARCHAR(256) NOT NULL);
