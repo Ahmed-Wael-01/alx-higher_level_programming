2d3d1a2dcf375e8346debba28424e2bbdcf6dcb1
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-06-18 20:21:43 -0700

    adv 1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 if (process.argv.length < 4) {
   console.log(0);
-}
+} else {
+  let biggest = 2;
+  let secondRunner;
 
-let biggest = 2;
-let secondRunner;
-
-for (let i = 2; i < process.argv.length; i++) {
-  if (parseInt(process.argv[i]) > parseInt(process.argv[biggest])) {
-    secondRunner = biggest;
-    biggest = i;
-    continue;
-  }
-  if ((isNaN(secondRunner) ||
+  for (let i = 2; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > parseInt(process.argv[biggest])) {
+      secondRunner = biggest;
+      biggest = i;
+      continue;
+    }
+    if ((isNaN(secondRunner) ||
       parseInt(process.argv[i]) > parseInt(process.argv[secondRunner])) && i !== biggest) {
-    secondRunner = i;
+      secondRunner = i;
+    }
   }
+  console.log(process.argv[secondRunner]);
 }
-console.log(process.argv[secondRunner]);
