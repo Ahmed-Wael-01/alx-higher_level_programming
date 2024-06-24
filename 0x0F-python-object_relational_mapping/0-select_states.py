e8bcd6ac14ca135409f6bb70b5293648c1800084
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-06-23 17:50:57 -0700

    one

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,11 @@
 """
 connects to db and lists all states
 """
+
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    """simple doc for testing"""
     db = MySQLdb.connect(
             host=127.0.0.1, port=3306, user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3])
