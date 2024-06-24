6635e1db8a90ea08e56f456b01f4ee028c292d1d
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-06-24 08:10:37 -0700

    fix three and four

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
             host="localhost", port=3306, user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-                                FROM cities JOIN states ON cities.state_id \
-                                = states.id ORDER BY cities.id ASC")
+    cur.execute("SELECT * FROM states\
+                WHERE BINARY name='{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
