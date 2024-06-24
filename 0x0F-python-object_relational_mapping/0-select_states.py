7b7caf4546146733901041e1461b1486d0b9d76f
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-06-23 17:29:12 -0700

    test zero

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,7 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    """simple doc for testing"""
     db = MySQLdb.connect(
             host=127.0.0.1, port=3306, user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3])
@@ -14,4 +15,3 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
