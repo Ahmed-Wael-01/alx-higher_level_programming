787a2994ef731b4b4ff7cd0c2f509dff87de7e96
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-06-24 18:47:31 -0700

    five

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
             host="localhost", port=3306, user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name\
+    cur.execute("SELECT cities.name, states.name\
                 FROM cities JOIN states ON cities.state_id=states.id\
                 WHERE BINARY states.name = %(name)s\
                 ORDER BY cities.id ASC", {'name': sys.argv[4]})
