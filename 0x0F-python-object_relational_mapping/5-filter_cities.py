c2e5c9dd95bc9888731f2c9350fc3b6d81205265
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-06-25 05:26:32 +0300

    try

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,10 +12,22 @@ if __name__ == '__main__':
             host="localhost", port=3306, user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name\
-                FROM cities JOIN states ON cities.state_id=states.id\
-                WHERE states.name = %(name)s\
-                ORDER BY cities.id ASC", {'name': sys.argv[4]})
+    cur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': sys.argv[4]
+        })
     rows = cur.fetchall()
     if rows is not None:
-        print(", ".join([row[0] for row in rows]))
+        print(", ".join([row[1] for row in rows]))
