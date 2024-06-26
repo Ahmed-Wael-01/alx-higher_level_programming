4b4051f39dd07e87f6fe3db1b145ccaf04ef5fd5
Ahmed Wael <xahmedwaheed@gmail.com>
2024-06-26 14:55:58 -0700

    eighth model

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all the state objects from the db
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).first()
+    if states is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print('Nothing')
