bb11189b79ae06a84b5d9a2236bb851052ac1ce1
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-07-30 23:14:53 +0300

    eight

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    python script to fetch given website
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    r = requests.post(
+            'http://0.0.0.0:5000/search_user',
+            data={'q': q})
+    try:
+        res_dict = r.json()
+        if len(res_dict) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(
+                res_dict.get('id'), res_dict.get('name')))
+    except:
+        print("Not a valid JSON")
