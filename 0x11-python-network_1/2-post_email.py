9f8e1590dea1b45d8e09deb19450e5ebb8af0853
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-07-30 11:46:47 +0300

    two

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    python script to fetch given website
+"""
+
+import urllib
+import sys
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+        print(body.decode("utf-8"))
