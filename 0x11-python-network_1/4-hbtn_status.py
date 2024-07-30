6aad945392f5f4c7436e6a299d75cf73309c4d5f
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-07-30 12:09:29 +0300

    four

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    python script to fetch given website
+"""
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
