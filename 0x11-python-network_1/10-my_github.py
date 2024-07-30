6f7406379db00575c3240eef261a8bade31a6315
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-07-30 23:44:16 +0300

    ten

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,13 +2,12 @@
 """
     python script to fetch given website
 """
-from requests.auth import HTTPBasicAuth
 import requests
 import sys
 
 if __name__ == "__main__":
-    basic = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get(
             'https://api.github.com/users/{}'.format(sys.argv[1]),
-            auth=basic)
+            auth=(sys.argv[1], sys.argv[2]))
+    print(sys.argv[1], sys.argv[2])
     print(r.json().get('id'))
