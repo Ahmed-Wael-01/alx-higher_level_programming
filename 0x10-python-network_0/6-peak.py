00ffd0a0b8eaf924b22b0d6ee5e2b07ff03f31dc
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-07-31 00:22:14 +0300

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds peak
+    """
+    lt = 0
+    rt = len(list_of_integers) - 1
+    while lt <= rt:
+        m = lt + ((rt - 1) // 2)
+        if m > 0 and list_of_integers[m] < list_of_integers[m - 1]:
+            rt = m - 1
+        if m < len(list_of_integers) - 1 and
+        list_of_integers[m] < list_of_integers[m + 1]:
+            lt = m + 1
+        else:
+            return list_of_integers[m]
