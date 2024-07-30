9357d56349290be26284d125e6c25a9db7f384a8
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-07-30 23:54:24 +0300

    million

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+lists last ten commits
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    coms = r.json()
+    for i in range(10):
+        print("{}: {}".format(
+                coms[i].get("sha"),
+                coms[i].get("commit").get("author").get("name")))
