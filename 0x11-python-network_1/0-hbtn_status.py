6cbe483db8d01b9c69126a2eb89232d287520780
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-07-30 11:26:57 +0300

    zero

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         con = res.read()
-        print(
-"""Body response:
+        print("""Body response:
 \t- type: {}
 \t- content: {}
 \t- utf8 content: {}""".format(
