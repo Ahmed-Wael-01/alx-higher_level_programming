3bcfa84192569e5ab7e1aec7ec95e50551e31257
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-08-20 07:27:33 -0700

    two

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log("code: " + response.statusCode);
+})
