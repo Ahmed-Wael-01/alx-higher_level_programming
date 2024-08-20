e8046e1aa2928c552445f70697a05f234ca5938f
Ahmed Wael <Xahmedwaheed@gmail.com>
2024-08-20 07:29:59 -0700

    two

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,5 +7,5 @@ request(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
   }
-  console.log("code: " + response.statusCode);
-})
+  console.log('code: ' + response.statusCode);
+});
